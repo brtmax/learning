56769bca3f2ea4c7b92211069d33bfdb7f1dff80
Max <maxbretschneider@mailbox.org>
2022-11-09 11:45:42 +0100

    Added model summary

--- gpc.py
+++ gpc.py
@@ -19,5 +19,21 @@ cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 
 # Define grid
 grid = dict()
-
 grid['kernel'] = [1*RBF(), 1*DotProduct(), 1*Matern(), 1*RationalQuadratic(), 1*WhiteKernel()]
+
+# Define search
+search = GridSearchCV(model, grid, scoring='accuracy', cv=cv, n_jobs=-1)
+
+# Perform the search
+results = search.fit(x, y)
+
+# Summarize results
+print('Best Mean Accuracy: %.3f' % results.best_score_)
+print('Best Config: %s' % results.best_params_)
+
+# Summarize all
+means = results.cv_results_['mean_test_score']
+params = results.cv_results_['params']
+
+for mean, param in zip(means, params):
+    print(">%.3f with: %r" % (mean, param))
