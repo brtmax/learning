1c4dfe06ed04ca3592586712bcfe7fb574713b8a
Max <maxbretschneider@mailbox.org>
2022-11-09 11:42:24 +0100

    Defined grid and model evaluation method

--- dataset.py
+++
@@ -1,7 +0,0 @@
-# test classification dataset
-from sklearn.datasets import make_classification
-
-# define dataset
-x, y = make_classification(n_samples=100, n_features=20, n_informative=15, n_redundant=5, random_state=1)
-
-print(x.shape, y.shape)
--- gpc.py
+++ gpc.py
@@ -1,6 +1,23 @@
-from sklearn.datasets import load_iris
+from sklearn.datasets import make_classification
+from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold
 from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF, DotProduct, Matern, RationalQuadratic, WhiteKernel
+
+
+from sklearn.datasets import load_iris
 from sklearn.gaussian_process.kernels import RBF
 
+# define dataset
+x, y = make_classification(n_samples=100, n_features=20, n_informative=15, n_redundant=5, random_state=1)
+
+# Define model
 # Gaussian Process Classifier
-model = GaussianProcessClassifier(kernel=1*RBF(1.0))
+model = GaussianProcessClassifier()
+
+# Define model evaluation method
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+# Define grid
+grid = dict()
+
+grid['kernel'] = [1*RBF(), 1*DotProduct(), 1*Matern(), 1*RationalQuadratic(), 1*WhiteKernel()]
