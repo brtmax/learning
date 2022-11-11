542873fbf115bc847b6fd87d519cc8fda181210c
Max <maxbretschneider@mailbox.org>
2022-11-11 23:38:07 +0100

    Implemented conv2d function

--- machine-vision/assignment2.py/assignment2.py
+++ machine-vision/assignment2.py/assignment2.py
@@ -15,3 +15,28 @@ print(gray_img.shape)
 
 # ensure image is float type
 gray_img = gray_img.astype(float)
+
+
+# implement convolution2d function
+def convolution2d(*, img, kernel) -> np.ndarray:
+    assert img.dtype == float, "Image must be in float"
+    assert kernel.dtype == float, "Kernel must be in float"
+    assert kernel.shape[0] == kernel.shape[1], "Kernel must be a square"
+    assert kernel.shape[0] % 1 == 1, "Kernel size must be uneven"
+    
+    # calculate size of padding
+    num_pad_pixels = (kernel.shape[0] - 1) / 2
+    
+    img_padded = copy.deepcopy(img)
+    img_padded = np.pad(img_padded, num_pad_pixels)
+    
+    img_result = np.zeros_like(img)
+    
+    # range over v dimension
+    for v in range(0,img.shape[0]):
+        # range over u dimension
+        for u in range(0, img.shape[1]):
+            patch = img_padded[u:kernel[0], v:kernel[1]]
+            
+            img_result[u:kernel[0], v:kernel[1]] = patch
+    return img_result
