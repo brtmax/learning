621b8d25137ab01e67979be237bb1a498b855a78
Max <maxbretschneider@mailbox.org>
2022-11-12 14:53:51 +0100

    Implemented method convert_acc_to_lines

--- machine-vision/assignment2/hough.py
+++ machine-vision/assignment2/hough.py
@@ -6,6 +6,7 @@ import copy
 import matplotlib
 import matplotlib.pyplot as plt
 from scipy.signal import convolve2d
+from skimage.feature import peak_local_max
 
 gray_img = skimage.io.imread("sources/triangle.png")
 plt.imshow(gray_img, cmap="gray")
@@ -29,15 +30,26 @@ def hough_transform_tc(edge_image: np.ndarray, ps: np.ndarray, e=0.5) -> np.ndar
         acc[lines_on_point_idx] += 1
     return acc
 
-def span_tc_parameter_space(*, c_max: float, shape=(200, 200)) -> np.ndarray:
+def span_tc_parameter_space(*, roh_max: float, shape=(200, 200)) -> np.ndarray:
     # Span values of theta over the desired range (-90.0 -> 90.0 degree in rad)
     theta = np.deg2rad(np.arange(-90, 90))
     # Span values of c over the desired range
-    c = np.arange(-c_max, c_max)
-    return np.meshgrid(theta, c)
+    roh = np.arange(-roh_max, roh_max)
+    return np.meshgrid(theta, roh)
+
+def local_peaks(acc, min_distance, num_peaks = 3):
+    return peak_local_max(acc, min_distance, num_peaks)
+    
+def convert_acc_to_lines(acc, ps, num_peaks = 3):
+    peaks = local_peaks(acc=acc, min_distance=0, num_peaks=num_peaks)
+    lines = []
+    for peak in peaks:
+        lines.append((peak[0], peak[1]))
+    return lines
+
 
 # span parameter space and calcualte accumulator
-t_c = span_tc_parameter_space(c_max=100, shape=(200,200))
+t_c = span_tc_parameter_space(roh_max=100, shape=(200,200))
 acc_tc = hough_transform_tc(edge_image=gray_img, ps=t_c)
 
 # visualize
@@ -45,8 +57,8 @@ fig, ax = plt.subplots()
 ax.imshow(acc_tc, cmap='gray')
 ax.set_yticks(np.linspace(0, acc_tc.shape[1]-1, 5))
 ax.set_yticklabels([-100,-50,0,50,100])
-ax.set_ylabel("c")
+ax.set_ylabel("Roh")
 ax.set_xticks(np.linspace(0, acc_tc.shape[1], 5))
 ax.set_xticklabels(np.round(np.deg2rad([-90,-45,0,45,90]), 2))
-ax.set_xlabel("t")
+ax.set_xlabel("Theta")
 plt.show()
